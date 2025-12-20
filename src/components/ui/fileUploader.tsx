dc6ba650c9b837f6432bb55cf23a0c6404bd7ea9
Shubham Singh <shubham1612singh99@gmail.com>
2025-12-20 11:48:52 +0530

    push

---
+++ src/components/ui/fileUploader.tsx
@@ -0,0 +1,14 @@
+import { Input } from "./input";
+
+const FileUploader = () =>{
+
+return (
+    <div>
+        <label>File Uploader</label>
+        <Input type="file" />
+    </div>
+)
+}
+
+
+export default FileUploader;
